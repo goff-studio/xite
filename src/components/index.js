6818b2bba35018f9dae0b88060ec5a9490fda372
Alireza Goudarzi <alireza@sdhouse.lt>
2022-03-31 07:18:32 +0300

    Components in components directory added to its index file

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './Text';
+export * from './View';
+export * from './ClickableLogo';
+export * from './HomeNewsItem';
+export * from './List';
+export * from './YearPicker';
+export * from './Modal';
